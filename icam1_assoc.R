bcbccb06351215d81c71ecc5bd5a2e55d2a44d8b
Broccolito <wanjungu001@gmail.com>
2025-04-09 15:20:28 -0700

    automatic commit

--- icam1_assoc.R
+++ icam1_assoc.R
@@ -1,9 +1,15 @@
 library(dplyr)
 library(data.table)
+library(ggplot2)
+library(ggpubr)
 
 geno = fread("genotype_master.csv")
 pheno = fread("cdp_phenotyping_master.csv")
 
 icam1 = select(geno, id, ICAM1_rs1799969G)
 
-icam1 = left_join(pheno, icam1, by = "id")
+icam1 = left_join(pheno, icam1, by = "id") |>
+  filter(genotype_exclude == 0)
+
+lm(data = icam1, formula = sbp_mean ~ ICAM1_rs1799969G + sex + bmi) |> 
+  summary()
